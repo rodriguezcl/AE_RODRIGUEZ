6b9419002765c270aa41ff3860d778915c9b8e23
rodriguezcl <cristianleonardorodriguez1991@gmail.com>
2023-02-25 21:46:16 -0300

    function ok

--- pages/js/amazing.js
+++ pages/js/amazing.js
@@ -1,3 +1,5 @@
+let container = document.getElementById("container")
+
 const data = {
     "currentDate": "2022-01-01",
     "events": [
@@ -171,3 +173,30 @@ const data = {
       }
     ]
   }
+
+
+const fragment = document.createDocumentFragment();
+
+function imprimirCards(array, contenedor){
+    for(let producto of array){
+        let div = document.createElement('div');
+        div.innerHTML += `<div class="col">
+        <div class="card h-100">
+        <img src="${producto.image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${producto.name}</h5>
+                        <p class="card-text">Category: ${producto.category}</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Price: $${producto.price}</small>
+                        <a type="button" class="btn btn-primary btn-sm" href="./pages/details.html">Ver mas</a>
+                    </div>
+                    </div>
+                </div>
+                ` 
+        fragment.appendChild(div)
+    }
+    contenedor.appendChild(fragment)
+}
+imprimirCards(data.events, container)
+// 
