c430b76cdfcdec368b1364a3850f1cf414a95386
rodriguezcl <cristianleonardorodriguez1991@gmail.com>
2023-02-26 18:24:24 -0300

    Home ok, upcoming events ok, past events ok

--- pages/js/upcomingEvents.js
+++ pages/js/upcomingEvents.js
@@ -1,25 +1,32 @@
 let container = document.getElementById("container")
 const fragment = document.createDocumentFragment();
 
-function imprimirCards(array, contenedor) {
+function checkUpcoming(array, contenedor) {
   for (let producto of array) {
-    let div = document.createElement('div');
-    div.innerHTML += `<div class="col">
-        <div class="card h-100">
-        <img src="${producto.image}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">${producto.name}</h5>
-                        <p class="card-text">Category: ${producto.category}</p>
-                    </div>
-                    <div class="card-footer">
-                        <small class="text-muted">Price: $${producto.price}</small>
-                        <a type="button" class="btn btn-primary btn-sm" href="./pages/details.html">See more</a>
-                    </div>
-                    </div>
-                </div>
-                `
-    fragment.appendChild(div)
+
+    let currentDate = data.currentDate;
+    let eventDate = producto.date;
+
+    if (currentDate < eventDate) {
+      let div = document.createElement('div');
+      div.innerHTML += `<div class="col">
+          <div class="card h-100">
+          <img src="${producto.image}" class="card-img-top" alt="...">
+                      <div class="card-body">
+                          <h5 class="card-title">${producto.name}</h5>
+                          <p class="card-text">Category: ${producto.category}</p>
+                      </div>
+                      <div class="card-footer">
+                          <small class="text-muted">Price: $${producto.price}</small>
+                          <a type="button" class="btn btn-primary btn-sm" href="./details.html">See more</a>
+                      </div>
+                      </div>
+                  </div>
+                  `
+      fragment.appendChild(div)
+
+    }
   }
   contenedor.appendChild(fragment)
 }
-imprimirCards(data.events, container)
+checkUpcoming(data.events, container)
