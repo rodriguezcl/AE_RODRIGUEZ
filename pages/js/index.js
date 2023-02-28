ccce63b67a9ecad1a726a1f317872dd3b2bbd7f3
rodriguezcl <cristianleonardorodriguez1991@gmail.com>
2023-02-28 09:40:16 -0300

    Funcion imprimirCards() ok

--- pages/js/index.js
+++ pages/js/index.js
@@ -2,24 +2,24 @@ let container = document.getElementById("container")
 const fragment = document.createDocumentFragment();
 
 function imprimirCards(array, contenedor) {
-  for (let producto of array) {
+  array.forEach((evento)=>{
     let div = document.createElement('div');
     div.innerHTML += `<div class="col">
         <div class="card h-100">
-        <img src="${producto.image}" class="card-img-top" alt="...">
+        <img src="${evento.image}" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">${producto.name}</h5>
-                        <p class="card-text">Category: ${producto.category}</p>
+                        <h5 class="card-title">${evento.name}</h5>
+                        <p class="card-text">Category: ${evento.category}</p>
                     </div>
                     <div class="card-footer">
-                        <small class="text-muted">Price: $${producto.price}</small>
+                        <small class="text-muted">Price: $${evento.price}</small>
                         <a type="button" class="btn btn-primary btn-sm" href="./pages/details.html">See more</a>
                     </div>
                     </div>
                 </div>
                 `
     fragment.appendChild(div)
-  }
+  })
   contenedor.appendChild(fragment)
 }
-imprimirCards(data.events, container)
+  imprimirCards(data.events, container)
