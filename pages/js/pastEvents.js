777ac0a983087767da13e269636822c9732d4aef
rodriguezcl <cristianleonardorodriguez1991@gmail.com>
2023-02-28 10:53:03 -0300

    filterUpcoming y filterPast funcionando ok

--- pages/js/pastEvents.js
+++ pages/js/pastEvents.js
@@ -1,32 +1,65 @@
+// let container = document.getElementById("container")
+// const fragment = document.createDocumentFragment();
+
+// function checkPast(array, contenedor) {
+//   for (let producto of array) {
+
+//     let currentDate = data.currentDate;
+//     let eventDate = producto.date;
+
+//     if (currentDate > eventDate) {
+//       let div = document.createElement('div');
+//       div.innerHTML += `<div class="col">
+//           <div class="card h-100">
+//           <img src="${producto.image}" class="card-img-top" alt="...">
+//                       <div class="card-body">
+//                           <h5 class="card-title">${producto.name}</h5>
+//                           <p class="card-text">Category: ${producto.category}</p>
+//                       </div>
+//                       <div class="card-footer">
+//                           <small class="text-muted">Price: $${producto.price}</small>
+//                           <a type="button" class="btn btn-primary btn-sm" href="./details.html">See more</a>
+//                       </div>
+//                       </div>
+//                   </div>
+//                   `
+//       fragment.appendChild(div)
+
+//     }
+//   }
+//   contenedor.appendChild(fragment)
+// }
+// checkPast(data.events, container)
+
+
+//Higher Order Function - .filter()
+
 let container = document.getElementById("container")
 const fragment = document.createDocumentFragment();
 
-function checkPast(array, contenedor) {
-  for (let producto of array) {
-
-    let currentDate = data.currentDate;
-    let eventDate = producto.date;
-
-    if (currentDate > eventDate) {
-      let div = document.createElement('div');
-      div.innerHTML += `<div class="col">
-          <div class="card h-100">
-          <img src="${producto.image}" class="card-img-top" alt="...">
-                      <div class="card-body">
-                          <h5 class="card-title">${producto.name}</h5>
-                          <p class="card-text">Category: ${producto.category}</p>
-                      </div>
-                      <div class="card-footer">
-                          <small class="text-muted">Price: $${producto.price}</small>
-                          <a type="button" class="btn btn-primary btn-sm" href="./details.html">See more</a>
-                      </div>
-                      </div>
-                  </div>
-                  `
-      fragment.appendChild(div)
-
-    }
-  }
+function imprimirCards(array, contenedor) {
+  array.forEach((evento)=>{
+    let div = document.createElement('div');
+    div.innerHTML += `<div class="col">
+        <div class="card h-100">
+        <img src="${evento.image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${evento.name}</h5>
+                        <p class="card-text">Category: ${evento.category}</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Price: $${evento.price}</small>
+                        <a type="button" class="btn btn-primary btn-sm" href="./details.html">See more</a>
+                    </div>
+                    </div>
+                </div>
+                `
+    fragment.appendChild(div)
+  })
   contenedor.appendChild(fragment)
 }
-checkPast(data.events, container)
+
+const filterPast = data.events.filter(evento => data.currentDate > evento.date)
+console.log(filterPast)
+
+imprimirCards(filterPast, container)
--- pages/js/upcomingEvents.js
+++ pages/js/upcomingEvents.js
@@ -1,32 +1,64 @@
+// let container = document.getElementById("container")
+// const fragment = document.createDocumentFragment();
+
+// function checkUpcoming(array, contenedor) {
+//   for (let producto of array) {
+
+//     let currentDate = data.currentDate;
+//     let eventDate = producto.date;
+
+//     if (currentDate < eventDate) {
+//       let div = document.createElement('div');
+//       div.innerHTML += `<div class="col">
+//           <div class="card h-100">
+//           <img src="${producto.image}" class="card-img-top" alt="...">
+//                       <div class="card-body">
+//                           <h5 class="card-title">${producto.name}</h5>
+//                           <p class="card-text">Category: ${producto.category}</p>
+//                       </div>
+//                       <div class="card-footer">
+//                           <small class="text-muted">Price: $${producto.price}</small>
+//                           <a type="button" class="btn btn-primary btn-sm" href="./details.html">See more</a>
+//                       </div>
+//                       </div>
+//                   </div>
+//                   `
+//       fragment.appendChild(div)
+
+//     }
+//   }
+//   contenedor.appendChild(fragment)
+// }
+// checkUpcoming(data.events, container)
+
+//Higher Order Function - .filter()
+
 let container = document.getElementById("container")
 const fragment = document.createDocumentFragment();
 
-function checkUpcoming(array, contenedor) {
-  for (let producto of array) {
-
-    let currentDate = data.currentDate;
-    let eventDate = producto.date;
-
-    if (currentDate < eventDate) {
-      let div = document.createElement('div');
-      div.innerHTML += `<div class="col">
-          <div class="card h-100">
-          <img src="${producto.image}" class="card-img-top" alt="...">
-                      <div class="card-body">
-                          <h5 class="card-title">${producto.name}</h5>
-                          <p class="card-text">Category: ${producto.category}</p>
-                      </div>
-                      <div class="card-footer">
-                          <small class="text-muted">Price: $${producto.price}</small>
-                          <a type="button" class="btn btn-primary btn-sm" href="./details.html">See more</a>
-                      </div>
-                      </div>
-                  </div>
-                  `
-      fragment.appendChild(div)
-
-    }
-  }
+function imprimirCards(array, contenedor) {
+  array.forEach((evento)=>{
+    let div = document.createElement('div');
+    div.innerHTML += `<div class="col">
+        <div class="card h-100">
+        <img src="${evento.image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${evento.name}</h5>
+                        <p class="card-text">Category: ${evento.category}</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Price: $${evento.price}</small>
+                        <a type="button" class="btn btn-primary btn-sm" href="./details.html">See more</a>
+                    </div>
+                    </div>
+                </div>
+                `
+    fragment.appendChild(div)
+  })
   contenedor.appendChild(fragment)
 }
-checkUpcoming(data.events, container)
+
+const filterUpcoming = data.events.filter(evento => data.currentDate < evento.date)
+console.log(filterUpcoming)
+
+imprimirCards(filterUpcoming, container)
