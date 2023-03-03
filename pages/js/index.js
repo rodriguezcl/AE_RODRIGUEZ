9f9447e555972c98b66e7e63163227951393009f
rodriguezcl <cristianleonardorodriguez1991@gmail.com>
2023-03-02 22:17:05 -0300

    Cambios index.js

--- pages/js/index.js
+++ pages/js/index.js
@@ -1,66 +1,96 @@
-//-------------- FUNCION IMPRIMIR CARDS ----------------
-
-let container = document.getElementById("container")
+const $container = document.getElementById('container');
 const fragment = document.createDocumentFragment();
+const $checkbox = document.getElementById('btn-group1');
+const $search = document.querySelector('input[placeholder="Search"]');
 
-function imprimirCards(array, contenedor) {
-  array.forEach((evento)=>{
-    let div = document.createElement('div');
-    div.innerHTML += `<div class="col">
-        <div class="card h-100">
-        <img src="${evento.image}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">${evento.name}</h5>
-                        <p class="card-text">Category: ${evento.category}</p>
-                    </div>
-                    <div class="card-footer">
-                        <small class="text-muted">Price: $${evento.price}</small>
-                        <a type="button" class="btn btn-primary btn-sm" href="./pages/details.html">See more</a>
-                    </div>
-                    </div>
-                </div>
-                `
-    fragment.appendChild(div)
-  })
-  contenedor.appendChild(fragment)
+const createCards = (array, contenedor) => {
+  contenedor.innerHTML = ""
+    array.forEach((evento)=>{
+      let div = document.createElement('div');
+      div.innerHTML += `<div class="col">
+          <div class="card h-100">
+          <img src="${evento.image}" class="card-img-top" alt="...">
+                      <div class="card-body">
+                          <h5 class="card-title">${evento.name}</h5>
+                          <p class="card-text">Category: ${evento.category}</p>
+                      </div>
+                      <div class="card-footer">
+                          <small class="text-muted">Price: $${evento.price}</small>
+                          <a type="button" class="btn btn-primary btn-sm" href="./pages/details.html">See more</a>
+                      </div>
+                      </div>
+                  </div>
+                  `
+      fragment.appendChild(div)
+    })
+    contenedor.appendChild(fragment)
+  }
+
+createCards(data.events, $container)
+
+//---------------------------------------------------------------------
+
+const createCategories = (array) =>{
+    let categories = array.map(category=> category.category)
+
+    categories = categories.reduce((acumulador, elemento)=>{
+        if(!acumulador.includes(elemento)){
+            acumulador.push(elemento);
+        }
+        return acumulador
+    }, [])
+    return categories 
+}
+let categories = createCategories(data.events)
+
+//---------------------------------------------------------------------
+
+const createChecks = (array, container) => {
+    array.forEach( category=>{
+      let div = document.createElement('div');
+          div.innerHTML += `<div class="category checks-container ${category.toLowerCase()}">
+                          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault ${category.toLowerCase()}">
+                          <label class="form-check-label" for="flexCheckDefault ${category.toLowerCase()}">
+                              ${category}
+                          </label>
+      
+                      </div>
+                      `
+          fragment.appendChild(div)
+        })
+        container.appendChild(fragment)
+      }
+createChecks(categories, $checkbox)
+
+//---------------------------------------------------------------------
+
+const filterSearch = (array, value) => {
+        let filteredArray = array.filter(element=> element.name.toLowerCase().includes(value.toLowerCase()))
+            return filteredArray
 }
 
-//---------- FIN FUNCION IMPRIMIR CARDS ---------
-
-
-  imprimirCards(data.events, container) //<-------- IMPRIME LAS CARDS
-
-  
-//-------------- FUNCION IMPRIMIR CHECKS ----------------
-
-let btnGroup = document.getElementById("btn-group1")
-
-function imprimirChecks(array, contenedor) {
-  array.forEach((evento)=>{
-    let div = document.createElement('div');
-    div.innerHTML += `<div class="category">
-                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault${evento}">
-                    <label class="form-check-label" for="flexCheckDefault${evento}">
-                        ${evento}
-                    </label>
-
-                </div>
-                `
-    fragment.appendChild(div)
-  })
-  contenedor.appendChild(fragment)
+const filterChecks = (array) => {
+    let checked = document.querySelectorAll('input[type="checkbox"]:checked');
+    console.log(checked)
+    //  let filteredArray = array.filter(element => element.category.toLowerCase().includes(checked.id.toLowerCase()))
+    //  return filteredArray
 }
 
-//------------FIN FUNCION IMPRIMIR CHECKS ---------------
+const filterAndPrint =  (array) =>{
+    let arrayFiltered = filterSearch(array, $search.value)
+    arrayFiltered = filterChecks(arrayFiltered)
+    return arrayFiltered
+}
 
-//---------- FILTRO CHECKS ---------------
+//  $search.addEventListener('keyup', (e) =>{
+//     let dataFilter = filterSearch(data.events, e.target.value)
+//     createCards(dataFilter, $container)
+// })
 
-const categories = data.events.map(evento => evento.category);
-const filterCategories = categories.filter((category, index) => categories.indexOf(category) === index);
-console.log(filterCategories);
-
-//-----------FIN FILTRO CHECKS ----------
-
-
-imprimirChecks(filterCategories, btnGroup) //<------ IMPRIME LOS CHECKS
+$checkbox.addEventListener('change', (e)=>{
+    // let dataFilter = filterChecks(data.events)
+    // createCards(dataFilter, $container)
+    console.log(e);
+    filterChecks (data.events)
+}) 
 
