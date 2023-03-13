aa232bcd996e82e1c6583aacce3da00751fc6477
rodriguezcl <cristianleonardorodriguez1991@gmail.com>
2023-03-13 17:17:08 -0300

    details.js funcionando ok

--- pages/js/details.js
+++ pages/js/details.js
@@ -1,13 +1,18 @@
-import { createDetails } from './funciones.js';
+import { createDetails } from "./funciones.js";
 
-const $container = document.getElementById('mainDetails');
-
-const queryString = location.search
-const params = new URLSearchParams(queryString)
-const beerID = params.get('id')
-const detail = data.events.find (element => element._id == beerID )
-
-console.log(detail)
-
-createDetails(detail, $container)
-
+async function getData() {
+  let apiURL = "../../assets/amazing.json";
+  fetch(apiURL)
+    .then((res) => res.json())
+    .then((res) => {
+      let data = res;
+      const $container = document.getElementById("mainDetails");
+      const queryString = location.search;
+      const params = new URLSearchParams(queryString);
+      const beerID = params.get("id");
+      const detail = data.events.find((element) => element._id == beerID);
+      createDetails(detail, $container);
+    })
+    .catch((err) => console.log(err));
+}
+getData();
